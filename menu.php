9efc062ffef406f8374619e92a5e9c7ec39015d1
chopper268 <162734656+chopper268@users.noreply.github.com>
2024-04-09 15:41:31 +0000

    Created file

---
+++ menu.php
@@ -0,0 +1,6 @@
+<?php
+echo '<a href="index.html">Home</a> -
+<a href="themes.html">Themes</a> -
+<a href="cs-tracks.html">Community showcase</a> -
+<a href="https://github.com/chopper268/">GitHub</a>'
+?>
