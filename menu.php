3c278b18aa76fe8997d19bb36804128cc12f4b42
chopper268 <162734656+chopper268@users.noreply.github.com>
2024-04-14 10:24:23 +0200

    Delete menu.php

--- menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<a href="index.html">Home</a> -
-<a href="themes.html">Themes</a> -
-<a href="cs-tracks.html">Community showcase</a> -
-<a href="https://github.com/chopper268/">GitHub</a>'
-?>
