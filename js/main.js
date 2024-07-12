feb98e0ac9eb5c68792cb88af887c04b09ef3479
chopper268 <162734656+chopper268@users.noreply.github.com>
2024-07-12 14:56:16 +0000

    Temporarily disabled page error

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-onerror = handleErr;
-var txt = "";
-function handleErr(msg, url, l) {
-    txt = "There was an error on this page.\n\n";
-    txt += "Error: " + msg + "\n"
-    txt += "URL: " + url + "\n"
-    txt += "Line: " + l + "\n\n"
-    txt += "Click OK to continue.\n\n"
-    alert(txt)
-    return true
-}
+//onerror = handleErr;
+//var txt = "";
+//function handleErr(msg, url, l) {
+    //txt = "There was an error on this page.\n\n";
+    //txt += "Error: " + msg + "\n"
+    //txt += "URL: " + url + "\n"
+    //txt += "Line: " + l + "\n\n"
+    //txt += "Click OK to continue.\n\n"
+    //alert(txt)
+    //return true
+//}
 
 function startHover() {
     document.getElementById("play-72726f5a-206f-4348-5232-206163636964").style.display = "flex";
